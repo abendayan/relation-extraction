32087e2297465e603f5409a97526f754665eefa7
Adele Bendayan <adele.bendayan@gmail.com>
2018-01-15 21:19:23 +0200

    eval finish

--- eval.py
+++ eval.py
@@ -10,53 +10,63 @@ def passed_time(previous_time):
     return round(time.time() - previous_time, 3)
 
 def read_annotation_files(name_file):
-    annotations = {}
+    annotations = []
     sentences = open(name_file, "r").read().split("\n")
     for sentence in sentences:
         if sentence != "":
             sentence = sentence.split("(")[0]
-            id_text = int(sentence.split("\t")[0].replace("sent", ""))
+            id_text = sentence.split("\t")[0].replace("sent", "")
             sentence = sentence.replace("sent"+ str(id_text) + "\t", "")
             if TARGET_TAG in sentence:
                 person, location = sentence.split(TARGET_TAG)
                 person = person.replace("\t", "")
                 location = location.replace("\t", "")
-                annotations[id_text] = {}
-                annotations[id_text]["person"] = person
-                annotations[id_text]["location"] = location
+                annotations.append(id_text + " " +  person + " " + location )
+                # annotations[id_text]["person"] = person
+                # annotations[id_text]["location"] = location
     return annotations
 
 def error_analysis(mistakes):
-    print "="*20
-    print "ANALISIS OF ERRORS"
-    print "="*20
     to_analyse = random.sample(mistakes, 5)
     for errors in to_analyse:
-        print "Sentence " + str(errors[0])
-        print "Gold : "
-        print errors[1]
-        print "Pred : "
-        print errors[2]
+        print "Relation :"
+        print errors
 
 
 def precision(gold_data, pred_data):
     good = 0.0
     bad = 0.0
     mistakes = []
-    for id_sentence, relation in gold_data.iteritems():
-        if id_sentence in pred_data:
-            if relation["person"] == pred_data[id_sentence]["person"] and relation["location"] == pred_data[id_sentence]["location"]:
-                good += 1
-            else:
-                bad += 1
-                mistakes.append([id_sentence, relation, pred_data[id_sentence]])
+    for pred in pred_data:
+        if pred in gold_data:
+            good += 1
         else:
             bad += 1
+            mistakes.append(pred)
     if ERROR_ANALYS:
+        print "="*20
+        print "ANALISIS OF ERRORS PRECISION (relations not in gold)"
+        print "="*20
         error_analysis(mistakes)
     return good/(good+bad)
 
-# TODO the recall
+def recall(gold_data, pred_data):
+    good = 0.0
+    bad = 0.0
+    mistakes = []
+    for gold in gold_data:
+        if gold in pred_data:
+            good += 1
+        else:
+            bad += 1
+            mistakes.append(gold)
+    if ERROR_ANALYS:
+        print "="*20
+        print "ANALISIS OF ERRORS RECALL (relations not in pred)"
+        print "="*20
+        error_analysis(mistakes)
+    return good/(good+bad)
+
 
 if __name__ == '__main__':
     gold_file = sys.argv[1]
@@ -65,3 +75,5 @@ if __name__ == '__main__':
     pred_data = read_annotation_files(predicted_file)
     prec = precision(gold_data, pred_data)
     print "Precision is " + str(prec)
+    rec = recall(gold_data, pred_data)
+    print "Recall is " + str(rec)
